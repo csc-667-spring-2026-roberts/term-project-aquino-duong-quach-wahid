bb6030a46da23dc2a4175d527cfc38e5ade0294e
Adrian Aquino <aaquino5@sfsu.edu>
2026-04-27 20:00:53 -0700

    fix views path for production

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "..", "src", "views"));
 
 app.set("trust proxy", 1);
 
